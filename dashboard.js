7a9d23b9f97a089077d25a8e977f0c654574eb6a
Sudx <sudx.oficial@gmail.com>
2025-07-14 14:32:37 -0300

    feat: configure Vercel Speed Insights

--- dashboard.js
+++ dashboard.js
@@ -1,6 +1,3 @@
-import { injectSpeedInsights } from '@vercel/speed-insights';
-injectSpeedInsights();
-
 // Waits for the DOM to be fully loaded before executing the script
 document.addEventListener('DOMContentLoaded', () => {
 
